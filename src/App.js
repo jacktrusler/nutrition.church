afd67d152e611fa6d5777bc9e9e39d3cc889bf27
Jack <jtrus93@gmail.com>
2022-01-31 15:49:43 -0500

    *.*

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import StartHere from "./components/js/StartHere";
+import Basics from "./components/js/Basics";
+import Macronutrients from "./components/js/Macronutrients";
+import Micronutrients from "./components/js/Micronutrients";
+import TheCell from "./components/js/TheCell";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> nutrition.church in progress...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/basics">
+              <Basics />
+            </Route>
+            <Route path="/">
+              <div className="start-here-container">
+                <div></div>
+              </div>
+              <div className="basics-container">
+                <a href="/basics">The Basics of Nutritional Science</a>
+              </div>
+              <div className="macronutrients-container">
+                <div>
+                  <a href="/macronutrients">Macronutrients</a>
+                </div>
+              </div>
+              <div className="micronutrients-container">
+                <div>
+                  <a href="/micronutrients">Micronutrients</a>
+                </div>
+              </div>
+              <div className="the-cell-container">
+                <div>
+                  <a href="/thecell">The Cell</a>
+                </div>
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
